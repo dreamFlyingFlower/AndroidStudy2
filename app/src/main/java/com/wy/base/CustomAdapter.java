97b4f47156d660c80488ec7ab7caddbea517ba48
wanyang <582822832@qq.com>
2018-04-03 22:12:59 +0800

    test

---
+++ app/src/main/java/com/wy/base/CustomAdapter.java
@@ -0,0 +1,19 @@
+package com.wy.base;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wy.R;
+
+/**
+ * 自定义一个适配器,继承自Adapter,未完成
+ * FIXME
+ */
+public class CustomAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_adapter);
+    }
+}
