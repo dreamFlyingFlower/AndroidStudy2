630e2a99e9af2dda2472679eeebe39a3e04502a4
wanyang <582822832@qq.com>
2018-04-10 13:42:46 +0800

    study

--- app/src/main/java/com/wy/base/ExBaseFuns.java
+++ app/src/main/java/com/wy/base/ExBaseFuns.java
@@ -29,6 +29,15 @@ public class ExBaseFuns extends AppCompatActivity {
 
         // 意图,activity之间的切换
         Intent intent = new Intent(ExBaseFuns.this,BaseActivity.class);
+        // 当需要给下一个activity中传递参数时,可以使用intent.putExtra往其中插入数据
+        intent.putExtra("test1","test1");
+        // Bundle类似于Map,键值对,android可传bundle但不能传递map
+        Bundle bundle = new Bundle();
+        bundle.putString("map1","map1");
+        intent.putExtra("bundle",bundle);
+        // 获得intent中的数据
+        String test1 = intent.getStringExtra("test1");
+        Bundle bundle1 = intent.getBundleExtra("bundle");
 
         // 拿到图片的字节数组之后显示到acitvity中,字节数组可以通过各种方式拿到
         // bitmap是位图,可以有多种方式显示图片
